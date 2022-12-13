85f6b6bb53d1604aa27fbeb9c3a9e6edda77b114
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-13 22:29:59 +0700

    tambah folder ?

--- src/components/general/Detail.jsx
+++ src/components/general/Detail.jsx
@@ -1,12 +1,17 @@
 // Import library
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import React from "react";
 
 const Detail = () => {
   return (
-    <div className="container">
-        <h1>Detail</h1>
+    <div>
+<h1>Example heading <span className="badge bg-secondary">New</span></h1>
+<h2>Example heading <span className="badge bg-secondary">New</span></h2>
+<h3>Example heading <span className="badge bg-secondary">New</span></h3>
+<h4>Example heading <span className="badge bg-secondary">New</span></h4>
+<h5>Example heading <span className="badge bg-secondary">New</span></h5>
+<h6>Example heading <span className="badge bg-secondary">New</span></h6>
     </div>
   );
 };
