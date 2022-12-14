4d0c53d58353b55624f90d7e0e702b8b0e1fb88a
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2022-12-14 20:51:35 +0700

    perbaikan

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,6 +4,9 @@ import Homepage from "../pages/Homepage";
 import Detail from "../pages/Detail";
 import ListRating from "../pages/ListRating";
 import ListRegion from "../pages/ListRegion";
+import UserSettings from "../pages/UserSettings";
+import UserRequest from "../pages/UserRequest";
+import AdminPost from "../pages/AdminPost";
 
 const Router = () => {
   return (
@@ -11,8 +14,11 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="Food/:id" element={<Detail />} />
-        <Route path="Food/:region" element={<ListRegion />} />
+        <Route path="Food/region/:region" element={<ListRegion />} />
         <Route path="Food/rating" element={<ListRating />} />
+        <Route path="User/setting" element={<UserSettings />} />
+        <Route path="User/request" element={<UserRequest />} />
+        <Route path="Admin/post" element={<AdminPost />} />
       </Routes>
     </BrowserRouter>
   );
