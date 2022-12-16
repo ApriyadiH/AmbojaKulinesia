66949ba6c82c8d2dd36c21471107bbe394389c82
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2022-12-16 15:10:36 +0700

    update

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -18,6 +18,9 @@ const Navbar = () => {
         <li>
           <Link to="/login">Login</Link>
         </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
       </ul>
     </div>
   );
