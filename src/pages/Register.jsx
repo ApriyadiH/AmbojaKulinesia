0db8d25becca8c0f8ec11839febeee2a6b4db385
Raka Aditya Kuncara <116072950+RakaAdityaKuncara15@users.noreply.github.com>
2022-12-16 15:03:09 +0700

    penambahan cek password di register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,10 +4,16 @@ function RegisterForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // check that the password and confirm password fields match
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
     // send a request to the server to create a new user account
     if (name && email && password) {
       // account successfully created, redirect the user to the login page
@@ -35,6 +41,11 @@ function RegisterForm() {
         <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
       </label>
       <br />
+      <label>
+        Confirm Password:
+        <input type="password" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} />
+      </label>
+      <br />
       {error && <p>{error}</p>}
       <button type="submit">Sign up</button>
     </form>
@@ -42,7 +53,3 @@ function RegisterForm() {
 }
 
 export default RegisterForm;
-
-
-
-
