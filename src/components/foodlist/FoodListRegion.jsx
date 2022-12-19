eb0db0f98e98145bd9a8c2315e3bf7f228fda2e8
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-19 22:39:06 +0700

    benerin ui

--- src/components/foodlist/FoodListRegion.jsx
+++ src/components/foodlist/FoodListRegion.jsx
@@ -1,49 +1,50 @@
 // Import library
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import styled from "styled-components";
 
 const FoodListRegion = () => {
-  const region = "West Java"
+  const {region} = useParams();
   const foods = [
     {
       postId:"1", 
       foodName: "Surabi Bandungggg ggggggggggggggggg ggggggggggg gggggggg gggg gggg g ggggggg",
       likes: "60",
-      image: "https://cdn-cas.orami.co.id/parenting/images/5-surabi.width-1000.jpg"
+      foodImage: "https://cdn-cas.orami.co.id/parenting/images/5-surabi.width-1000.jpg"
     },
     {
       postId:"2",
       foodName: "Batagor",
       likes: "90",
-      image: "https://cdn-cas.orami.co.id/parenting/images/makanan_khas_Sunda-batagor.width-1000.jpg"
+      foodImage: "https://cdn-cas.orami.co.id/parenting/images/makanan_khas_Sunda-batagor.width-1000.jpg"
     },
     {
       postId:"3",
       foodName: "Cimol",
       likes: "60",
-      image: "https://cdn-cas.orami.co.id/parenting/images/2-cilok.width-1000.jpg"
+      foodImage: "https://cdn-cas.orami.co.id/parenting/images/2-cilok.width-1000.jpg"
     },
     {
       postId:"4",
       foodName: "Mie Kocok Bandung",
       likes: "40",
-      image: "https://cdn-cas.orami.co.id/parenting/images/mie_kocok2.width-1000.jpg"
+      foodImage: "https://cdn-cas.orami.co.id/parenting/images/mie_kocok2.width-1000.jpg"
     },
     {
       postId:"5",
       foodName: "Seblak",
       likes: "70",
-      image: "https://cdn-cas.orami.co.id/parenting/images/seblak_1.width-1000.jpg"
+      foodImage: "https://cdn-cas.orami.co.id/parenting/images/seblak_1.width-1000.jpg"
     },
     {
       postId:"6",
       foodName: "Bandros",
       likes: "20",
-      image: "https://cdn-cas.orami.co.id/parenting/images/Bandros.width-1000.jpg"
+      foodImage: "https://cdn-cas.orami.co.id/parenting/images/Bandros.width-1000.jpg"
     },
   ]
 
@@ -64,24 +65,24 @@ const FoodListRegion = () => {
   return (
     <StContainer className="container-fluid">
       <StCardTitle>
-        <h4>{region}</h4>
+        <h4>{region.toLowerCase().split('_').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')}</h4>
       </StCardTitle>
       <StCardContainer>
         <StScrollBar>
           <div className="container-fluid">
-            <div className="row">
-              {foods.map((food) => {
+            <div className="row row-cols-md-4">
+              {foods.map((food,index) => {
                 return (
-                  <div className="container col-3 p-2">
-                    <StCard className="container">
+                  <div className="p-2" key={food.postId}>
+                    <StCard className="col">
                       <div className="row">
                         <div className="col-md-6">
-                          <StImage src={food.image} className="d-block w-100" alt={food.foodName}/>
+                          <StImage src={food.foodImage} alt={food.foodName}/>
                         </div>
                         <StNameLikes className="col-md-6">
-                          <StName>
-                            <h5 className="text=truncate">{food.foodName}</h5>
-                          </StName>
+                          <div>
+                            <StName>{food.foodName}</StName>
+                          </div>
                           <div className="row">
                             <i className="bi bi-heart-fill mx-2 col-1" style={{color:"red"}}></i>
                             <p className="col">{food.likes}</p>
@@ -148,15 +149,21 @@ const StImage = styled.img`
   width:100%;
   height: 30vh;
   object-fit: cover; 
+
+  border-radius: 13px;
+  
+  display: block;
 `
 
 const StNameLikes = styled.div`
   
 `
 
-const StName = styled.span`
+const StName = styled.h5`
+  padding-bottom: 10px;
   overflow: hidden; 
-  white-space:nowrap;
   text-overflow: ellipsis;
-  max-width:0px;
+  display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
   `
