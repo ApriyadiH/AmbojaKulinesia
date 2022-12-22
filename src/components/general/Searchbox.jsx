366dab1b2995ca79e75291ad7ae7e6af628e8785
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-22 14:18:42 +0700

    tambah search box, tapi blum ada fitur

--- src/components/general/Searchbox.jsx
+++ src/components/general/Searchbox.jsx
@@ -1,16 +1,30 @@
 // Import library
+import React from "react";
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import React from "react";
+import styled from "styled-components";
 
 const Searchbox = () => {
   return (
-    <div className="container">
-        <h1>Search box</h1>
-        <p><input type="text" placeholder="Search here"/></p>
-        <button type="button" className="btn btn-outline-primary">Search</button>
+    <div className="container-fluid d-flex flex-row-reverse bg-light">
+        <StInput type="text" placeholder="Search here"></StInput>
+        <StButton type="button"><i class="bi bi-search"></i></StButton>
     </div>
   );
 };
 
 export default Searchbox;
+
+const StInput = styled.input`
+  width: 30vh;
+  padding: 5px 15px;
+  margin: 10px;
+  border: none;
+  border-radius: 10px;  
+`
+
+const StButton = styled.button`
+  border: none;
+  background-color: transparent;
+`
