efec150ace56fa07eff8d1c94cdfc4805ef5486b
annisana12 <98462398+annisana12@users.noreply.github.com>
2022-12-22 13:06:14 +0700

    admin post list beres

--- src/components/postRequest/AdminPostLists.jsx
+++ src/components/postRequest/AdminPostLists.jsx
@@ -1,99 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import './UserRequestList.css';
 import { useDispatch } from 'react-redux';
 import { editPost } from '../../redux/modules/editPostSlice';
+import { useState } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AdminPostsLists = () => {
     const dispatch = useDispatch();
 
-    // ini nanti diganti pakai axios get, status approved aja
-    const postLists = [
-        {
-            postId: 'xuhd272dkwjkfh',
-            foodName: 'Pempek',
-            region: 'South Sumatra',
-            imageUrls: [
-                'https://www.piknikdong.com/wp-content/uploads/2021/02/Cara-Membuat-Pempek.jpg',
-                'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/09/04/4062442039.jpeg',
-                'https://img.okezone.com/content/2020/12/03/298/2321042/resep-pempek-dos-udang-rebon-rasanya-jos-n1FGhLyt0x.jpg'
-            ],
-            description: 'Pempek is a savoury Indonesian fish cake delicacy, made of fish and tapioca, from Palembang, South Sumatra.'
-        },
-        {
-            postId: 'xsa27dhsj28d',
-            foodName: 'Nasi Pecel Madiun',
-            region: 'East Java',
-            imageUrls: [
-                'url1',
-                'url2'
-            ],
-            description: 'Nasi pecel is made from rice, vegetables, peanut sauce, and soybeancake.'
-        },
-        {
-            postId: 'daqjj381qsqad',
-            foodName: 'Batagor',
-            region: 'West Java',
-            imageUrls: [
-                'url1',
-                'url2'
-            ],
-            description: 'Batagor is well known food in Bandung.'
-        },
-        {
-            postId: 'wqhuwq82u092',
-            foodName: 'Brem',
-            region: 'East Java',
-            imageUrls: [
-                'url1',
-                'url2'
-            ],
-            description: 'Brem is wellknown snack from Madiun.'
-        },
-        {
-            postId: 'jaquh937hdd',
-            foodName: 'Cilok',
-            region: 'West Java',
-            imageUrls: [
-                'url1',
-                'url2'
-            ],
-            description: 'Cilok is well known snack from Sundaneese.'
-        },
-        {
-            postId: 'uq2h91hdwf3',
-            foodName: 'Bolu Lembang',
-            region: 'West Java',
-            imageUrls: [
-                'url1',
-                'url2',
-                'url3'
-            ],
-            description: 'Bolu lembang is cake from Lembang, Bandung.'
-        },
-        {
-            postId: 'jadnjkdwquoi',
-            foodName: 'Brownis Amanda',
-            region: 'West Java',
-            imageUrls: [
-                'url1',
-                'url2'
-            ],
-            description: 'Brownis is well known cake for Indonesian people specifically in Java'
-        },
-        {
-            postId: 'ashqhhwff',
-            foodName: 'Rendang',
-            region: 'West Sumatra',
-            imageUrls: [
-                'url1',
-                'url2'
-            ],
-            description: "Rendang is delicious meat that become most of Indonesian people's favorite. "
-        }
-    ];
+    const [postLists, setPostLists] = useState([]);
+    const [errMessage, setErrMessage] = useState('');
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = user.token;
+
+    const config = {
+        headers: { Authorization: `Bearer ${token}` }
+    };
+
+    useEffect(() => {
+        axios.get(
+            "https://ambojakulinesiaserver.vercel.app/admin/post",
+            // "http://localhost:8000/api/admin/post",
+            config
+        ).then((response) => {
+            const { data } = response.data
+            console.log(response)
+            setPostLists(data);
+            setErrMessage('');
+        }).catch((error) => {
+            setErrMessage(error.response.data.message);
+        })
+    });
 
     const editPostHandler = (req) => {
         dispatch(editPost({
@@ -106,7 +49,28 @@ const AdminPostsLists = () => {
     };
 
     const deletePost = (postId) => {
-        // axios delete disini
+        axios.delete(
+            "https://ambojakulinesiaserver.vercel.app/post",
+            // "http://localhost:8000/api/post",
+            {
+                data: { postId: postId },
+                headers: { Authorization: `Bearer ${token}` }
+            }
+        ).then((response) => {
+            toast.success(response.data.message, {
+                position: "top-right",
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+            setErrMessage('');
+        }).catch((error) => {
+            setErrMessage(error.response.data.message);
+        })
     };
 
     return (
@@ -115,18 +79,20 @@ const AdminPostsLists = () => {
                 <h5>Post List</h5>
             </div>
             <div className='userRequest-body admin-post'>
+                {errMessage && <div className='errormessage3'>{errMessage}</div>}
                 <div className='adminpost-map'>
                     {postLists.map((request) => (
                         <div className='content-row adminpost-cntn' key={`id-${request.postId}`}>
                             <div>{request.foodName}</div>
                             <div className='usereditdel'>
                                 <i className="bi bi-pencil-square" onClick={() => { editPostHandler(request) }}></i>
-                                <i className="bi bi-trash" onClick={() => {deletePost(request.postId)}}></i>
+                                <i className="bi bi-trash" onClick={() => { deletePost(request.postId) }}></i>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
+            <ToastContainer />
         </div>
     )
 };
